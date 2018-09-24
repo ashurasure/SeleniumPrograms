ae550bd19b4e99dda16f97af359d41829befc94e
ashurasure <ashurasure12@gmail.com>
2018-09-25 01:18:49 +0530

    changes made in propertiesFileEx class

--- MyProject/src/excelutility/PropertiesFileEx.java
+++ MyProject/src/excelutility/PropertiesFileEx.java
@@ -6,15 +6,16 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class PropertiesFileEx {
-	public static void getproperty() throws Exception {
-		Properties p=new Properties();
-		InputStream input=new FileInputStream("E:\\input.properties");
-		p.load(input);
-		System.out.println(p.getProperty("browser"));
-	}
+	//public static void getproperty() throws Exception {
+		
+	
 public static void main(String[] args) throws Exception {
-	PropertiesFileEx pe= new PropertiesFileEx();
+	Properties p=new Properties();
+	InputStream input=new FileInputStream("E:\\input.properties");
+	p.load(input);
+	System.out.println(p.getProperty("browser"));
+	/*PropertiesFileEx pe= new PropertiesFileEx();
 	pe.getproperty();
-	getproperty();
+	getproperty();*/
 }
 }
